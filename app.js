cf223d8c96a564a98fea012f289e18caba96e78a
Anthony Villa <markv535@icloud.com>
2022-10-24 20:13:30 -0700

    uploaded files

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("Let's get this party started!");
+
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+/* use ajax result to add a gif */
+
+function addGif(res) {
+    try {
+        let numResults = res.data.length;
+        if (numResults) {
+            let randomIdx = Math.floor(Math.random() * numResults);
+            let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+            let $newGif = $("<img>", {
+            src: res.data[randomIdx].images.original.url,
+            class: "w-100"
+            });
+            $newCol.append($newGif);
+            $gifArea.append($newCol);
+        }
+    } catch (error) {
+        console.log(error);
+        alert("GIF NOT FOUND!");
+        }
+    }
+
+/* handle form submission: clear search box & make ajax call */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "e8HkJCZ7TE9vjCGnAoh5L30dN31DoKrm"
+    }
+  });
+  addGif(response.data);
+});
+
+/* remove gif */
+
+$("#remove").on("click", function() {
+  $gifArea.empty();
+});
