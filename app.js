a93b6473555d27a5b40f961dfd10739cd23d47d3
Anthony Villa <markv535@icloud.com>
2022-10-24 20:30:56 -0700

    updated https on Giphy link

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ $("form").on("submit", async function(evt) {
   let searchTerm = $searchInput.val();
   $searchInput.val("");
 
-  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+  const response = await axios.get("https://api.giphy.com/v1/gifs/search", {
     params: {
       q: searchTerm,
       api_key: "e8HkJCZ7TE9vjCGnAoh5L30dN31DoKrm"
